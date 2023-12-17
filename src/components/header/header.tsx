ab7bc3a7f57bafc81371bf93359602c039df4fcd
tbosmans <tom.94.bosmans@gmail.com>
2023-12-17 19:09:56 +0100

    Setup github oauth.

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,5 +1,26 @@
+import { Form } from "@builder.io/qwik-city";
 import { component$ } from "@builder.io/qwik";
+import { useAuthSession, useAuthSignout } from "~/routes/plugin@auth";
 
 export default component$(() => {
-  return <header></header>;
+  const signOut = useAuthSignout();
+  const session = useAuthSession();
+
+  return (
+    <nav>
+      <ul>
+        <li>
+          <strong>{session.value?.user?.email}</strong>
+        </li>
+      </ul>
+      <ul>
+        <li>
+          <Form action={signOut}>
+            <input type="hidden" name="callbackUrl" value="/sign_in" />
+            <button>Sign Out</button>
+          </Form>
+        </li>
+      </ul>
+    </nav>
+  );
 });
---
+++ src/routes/sign_in/index.tsx
@@ -0,0 +1,6 @@
+import { component$ } from "@builder.io/qwik";
+import GithubSignIn from "~/components/auth/github-sign-in";
+
+export default component$(() => {
+  return <GithubSignIn />;
+});
